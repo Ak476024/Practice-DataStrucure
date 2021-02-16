b3df4e2041ad54e330f21f6c70ca0d7dc9a3703d
Ankit Kumar <ankit.jagdish@elucidata.io>
2021-02-16 21:54:49 +0530

    Implemeted Stack

---
+++ Stack.java
@@ -0,0 +1,38 @@
+public class Stack {
+  QNode<String>front;
+
+  public Stack(){
+    this.front=null;
+  }
+
+  public void push(String key){
+    QNode<String> node = new QNode<String>(key);
+    if(front==null){
+      this.front = node;
+      return;
+    }
+    node.next= this.front;
+    this.front= node;
+  }
+
+  public void pop(){
+    if ( this.front==null) return;
+    this.front= this.front.next;
+  }
+
+  public String peek(){
+    return this.front.data;
+  }
+
+  public static void main(String[] args){
+    Stack s = new Stack();
+    s.push("A");
+    s.push("B");
+    s.push("C");
+    s.pop();
+    System.out.println(s.peek());
+
+  }
+
+
+}
